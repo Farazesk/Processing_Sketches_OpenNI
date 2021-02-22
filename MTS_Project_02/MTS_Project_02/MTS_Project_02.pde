faadddc9e35da1e43b3a771e60ee58474083aeea
Faraz.Eskandari <faraz.esk@gmail.com>
2021-02-22 16:55:50 +0330

    This is first test for OpenNI and kinect sensor

---
+++ MTS_Project_02/MTS_Project_02/MTS_Project_02.pde
@@ -0,0 +1,19 @@
+import SimpleOpenNI.*;
+
+SimpleOpenNI kinect;
+
+void setup() {
+    size(1280, 640);
+    kinect = new SimpleOpenNI(this);
+
+    kinect.enableDepth();
+    kinect.enableRGB();
+}
+
+void draw() {
+    kinect.update();
+
+    image(kinect.depthImage(), 0, 0);
+    image(kinect.rgbImage(), 640, 0);
+}
+
---
+++ MTS_Project_02/MTS_Project_02/out/source/MTS_Project_02.java
@@ -0,0 +1,47 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import SimpleOpenNI.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class MTS_Project_02 extends PApplet {
+
+
+
+SimpleOpenNI kinect;
+
+public void setup() {
+    
+    kinect = new SimpleOpenNI(this);
+
+    kinect.enableDepth();
+    kinect.enableRGB();
+}
+
+public void draw() {
+    kinect.update();
+
+    image(kinect.depthImage(), 0, 0);
+    image(kinect.rgbImage(), 640, 0);
+}
+
+  public void settings() {  size(1280, 640); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "MTS_Project_02" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
